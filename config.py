25fb7c5caaef7b276ffd7b2b897f99d11a120544
Davi Castro <davicastroti@hotmail.com>
2021-07-30 20:26:45 -0300

    Created configuration needed to authentication

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
-public_key = 'Your public key'
+import datetime as dt
+import hashlib
+
+ts = str(dt.datetime.now().timestamp())
+apikey = 'Your public key'
 private_key = 'Your private key'
+
+encoder = hashlib.md5()
+encoder.update(ts.encode('utf8'))
+encoder.update(private_key.encode('utf8'))
+encoder.update(apikey.encode('utf8'))
+
+hash = encoder.hexdigest()
