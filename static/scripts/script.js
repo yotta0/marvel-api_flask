1012aa6973141585f2c6a1efdff26c747849d525
Davi Castro <davicastroti@hotmail.com>
2021-07-30 15:18:27 -0300

    Created function toogleMenu() to change class to .active

---
+++ static/scripts/script.js
@@ -0,0 +1,6 @@
+function toogleMenu(){
+    var menuToogle = document.getElementById("menutoggle");
+    var navigation = document.getElementById("navigationId");
+    menuToogle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
