8320c1407b2aa38142d37290366fbb2373515212
Davi Castro <davicastroti@hotmail.com>
2021-07-30 22:30:42 -0300

    Added endpoint  and function to  return  query

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
-from flask import render_template
+from config import ts, apikey, hash
+import requests
+from flask import Flask, render_template
+
+def get_chr():
+    endpoint = ('https://gateway.marvel.com/v1/public/characters?')
+
+    try:
+        data_chr = requests.get(f'{endpoint}ts={ts}&apikey={apikey}&hash={hash}&limit=1').json()  # noqa: E501
+
+        name = data_chr["data"]["results"][0]["name"]
+        description = data_chr["data"]["results"][0]["description"]
+        thumbnail = data_chr["data"]["results"][0]["thumbnail"]["path"]
+        extension = data_chr["data"]["results"][0]["thumbnail"]["extension"]
+        # Format URL for image from resp.
+        thumbnail = f"{thumbnail}/landscape_incredible.{extension}"
+
+        return {"name": name, "description": description, "thumbnail": thumbnail}
+    except IndexError:
+        return render_template("index.html")
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
