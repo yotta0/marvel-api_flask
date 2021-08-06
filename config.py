f38db7d008f78902106b1ee6dcdebf0cd3f58b4a
Davi Castro <davicastroti@hotmail.com>
2021-08-06 03:02:12 -0300

    typo

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import hashlib
 ts = str(dt.datetime.now().timestamp())
 apikey = os.environ.get("API_KEY")
 private_key = os.environ.get("PRIVATE_KEY")
+
 encoder = hashlib.md5()
 encoder.update(ts.encode('utf8'))
 encoder.update(private_key.encode('utf8'))
