b17a1e1a1c1b6536841aa74c41f678e4a5d3fcb0
Davi Castro <davicastroti@hotmail.com>
2021-08-06 03:01:03 -0300

    Filter json to fetch some specific fields

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 from config import ts, apikey, hash
-import requests
-from flask import Flask, render_template
-
-def get_chr():
-    endpoint = ('https://gateway.marvel.com/v1/public/characters?')
-
-    try:
-        data_chr = requests.get(f'{endpoint}ts={ts}&apikey={apikey}&hash={hash}&limit=1').json()  # noqa: E501
-
-        name = data_chr["data"]["results"][0]["name"]
-        description = data_chr["data"]["results"][0]["description"]
-        thumbnail = data_chr["data"]["results"][0]["thumbnail"]["path"]
-        extension = data_chr["data"]["results"][0]["thumbnail"]["extension"]
-        # Format URL for image from resp.
-        thumbnail = f"{thumbnail}/landscape_incredible.{extension}"
-
-        return {"name": name, "description": description, "thumbnail": thumbnail}
-    except IndexError:
-        return render_template("index.html")
+import requests as req
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+
+    chr_name = 'hulk'
+    endpoint = ('https://gateway.marvel.com/v1/public/characters?')
+    params = (f'name={chr_name}&ts={ts}&apikey={apikey}&hash={hash}&limit=1')
+
+    r = req.get(f'{endpoint}{params}').json()
+
+    # Variables to treat thumbnails and his extension
+    thumbnail = r["data"]["results"][0]["thumbnail"]["path"]
+    extension = r["data"]["results"][0]["thumbnail"]["extension"]
+
+    data = {
+        "name": r["data"]["results"][0]["name"],
+        "description": r["data"]["results"][0]["description"],
+        "thumbnail": f"{thumbnail}/landscape_incredible.{extension}"
+    }
+
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
