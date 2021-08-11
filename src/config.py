9b4c114ea1a00a8b9cbe0e010ae993d0b339a3a3
Davi Castro <davicastroti@hotmail.com>
2021-08-11 01:33:50 -0300

    Refact to flask directory strutucture

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+import datetime as dt
+import hashlib
+
+ts = str(dt.datetime.now().timestamp())
+apikey = os.environ.get("API_KEY")
+private_key = os.environ.get("PRIVATE_KEY")
+
+encoder = hashlib.md5()
+encoder.update(ts.encode('utf8'))
+encoder.update(private_key.encode('utf8'))
+encoder.update(apikey.encode('utf8'))
+
+hash = encoder.hexdigest()
